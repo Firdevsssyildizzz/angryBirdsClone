b87c4a46914dfee58462abc5e358c3903cfa71bf
Firdevsssyildizzz <128640413+Firdevsssyildizzz@users.noreply.github.com>
2023-09-23 22:08:02 +0300

    Add files via upload

---
+++ GameViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AngryBirds
+//
+//  Created by Firdevs Yildiz on 22/08/2023
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                // Present the scene
+                view.presentScene(scene)
+            }
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+            view.showsPhysics = true
+            view.showsDrawCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
